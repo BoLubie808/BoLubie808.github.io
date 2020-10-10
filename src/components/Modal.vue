45834a1c77af379beb61307c9038a77e632f89c6
Kacper Gajda <djwielmozny@gmail.com>
2020-10-10 16:22:01 +0200

    commit

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,18 +1,18 @@
 <template>
-<div class="outerWrapper">
-  <div class="innerWrapper">
-    <div class="photo">
-      <img :src="photo">
+  <div class="outerWrapper">
+    <div class="innerWrapper">
+      <div class="photo">
+        <img :src="photo">
+      </div>
+      <div class="description">
+        <h2 class="title">{{ title }}</h2>
+        <p class="description">
+          {{ description }}
+        </p>
+      </div>
     </div>
-    <div class="description">
-      <h2 class="title">{{ title }}</h2>
-      <p class="description">
-        {{ description }}
-      </p>
-    </div>
+    <div class="close" @click="$emit('closeModal')" />
   </div>
-  <div class="close" @click="$emit('closeModal')" />
-</div>
 </template>
 <script>
 export default {
@@ -38,7 +38,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.outerWrapper {
+  .outerWrapper {
     background: #f6f6f6;
     max-width: 100%;
     height: 100%;
@@ -46,17 +46,17 @@ export default {
     top: 0;
     left: 0;
     @media (min-width: 1024px) {
-        max-width: 70%;
-        height: 60%;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        margin: auto;
-        box-shadow: 0 30px 30px -10px rgba(0,0,0, .3);
+      max-width: 70%;
+      height: 65%;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      margin: auto;
+      box-shadow: 0 30px 30px -10px rgba(0,0,0, .3);
     }
-}
-.close {
+  }
+  .close {
     position: absolute;
     width: 30px;
     height: 30px;
@@ -64,25 +64,25 @@ export default {
     right: 0;
     top: 0;
     cursor: pointer;
-    &::after,
-    &::before {
-        position: absolute;
-        top: 30px;
-        right: 20px;
-        content: '';
-        width: 20px;
-        height: 2px;
-        background: black;
-        display: block;
+    &::before,
+    &::after {
+      position: absolute;
+      top: 30px;
+      right: 20px;
+      content: '';
+      width: 20px;
+      height: 2px;
+      background: black;
+      display: block;
     }
     &::before {
-        transform: rotate(45deg);
+      transform: rotate(45deg);
     }
     &::after {
-        transform: rotate(-45deg);
+      transform: rotate(-45deg);
     }
-}
-.innerWrapper {
+  }
+  .innerWrapper {
     display: flex;
     height: 100%;
     padding: 50px;
@@ -90,25 +90,25 @@ export default {
     align-items: center;
     flex-direction: column;
     @media (min-width: 1024px) {
-        flex-direction: row;
-        .photo {
-            min-width: 50%;
-            margin-right: 20px;
-        }
+      flex-direction: row;
+      .photo {
+        min-width: 50%;
+        margin-right: 20px;
+      }
     }
     .photo {
+      width: 100%;
+      height: auto;
+      background: black;
+      img {
         width: 100%;
-        height: auto;
-        background: black;
-        img {
-            width: 100%;
-        }
+      }
     }
     .description {
-        color: #333;
+      color: #333;
     }
     .title {
-        color: #1e3d4a;
+      color: #1e3d4a;
     }
-}
+  }
 </style>
